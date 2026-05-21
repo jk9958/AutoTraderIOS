bfca90f042bceb7dce8ac9bfaf127c08d95d7b14
jk <jk9958@gmail.com>
2026-05-21 16:55:48 +0300

    Basic setup with tailgate network

---
+++ AutoTraderIOS/AutoTraderIOSTests/AutoTraderIOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  AutoTraderIOSTests.swift
+//  AutoTraderIOSTests
+//
+//  Created by DakshinAJK on 21/05/2026.
+//
+
+import Testing
+
+struct AutoTraderIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
